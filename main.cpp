0e075f662525ebef43a21e56fbb1cac1971f11ce
Emanuele Luca Cerea <cereaema@gmail.com>
2026-02-13 13:21:30 +0100

    Added chording!

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ int main() {
 				expandBoard(x, y, board);
 			}
 		}
-		flag = 1;
+		flag = 2;
 		if (win == 0) {
 			flag = 0;
 			for (int i = 0; i < height && flag != 1; i++) {
@@ -58,12 +58,12 @@ int main() {
 			}
 		}
 		if (flag == 0) {
-			flag = 1;
+			flag = 3;
 			win = 1;
 		}
 	} while (win != 1);
 	printBoard(board, 1);
-	if (flag == 1) {
+	if (flag == 3) {
 		cout << "YOU WIN!" << endl;
 	}
 	else {
@@ -114,31 +114,63 @@ void printBoard(int board[width][height], int lose) {
 				cout << "🟨";
 			}
 			else {
-				switch (board[i][j]) { // print with colors
-					case 1:
-						cout << "\e[0;34m";
-						break;
-					case 2:
-						cout << "\e[0;32m";
-						break;
-					case 3:
-						cout << "\e[0;31m";
-						break;
-					case 4:
-						cout << "\e[0;94m";
-						break;
-					case 5:
-						cout << "\e[0;91m";
-						break;
-					case 6:
-						cout << "\e[0;36m";
-						break;
-					case 7:
-						cout << "\e[38;5;240m";
-						break;
-					case 8:
-						cout << "\e[38;5;245m";
-						break;
+				if (board[i][j] <= 20) {
+					switch (board[i][j]) { // print with colors
+						case 1:
+							cout << "\e[0;34m";
+							break;
+						case 2:
+							cout << "\e[0;32m";
+							break;
+						case 3:
+							cout << "\e[0;31m";
+							break;
+						case 4:
+							cout << "\e[0;94m";
+							break;
+						case 5:
+							cout << "\e[0;91m";
+							break;
+						case 6:
+							cout << "\e[0;36m";
+							break;
+						case 7:
+							cout << "\e[38;5;240m";
+							break;
+						case 8:
+							cout << "\e[38;5;245m";
+							break;
+					}
+				}
+				else {
+					board[i][j] -= 20;
+					switch (board[i][j]) { // print with colors
+						case 1:
+							cout << "\e[0;44m";
+							break;
+						case 2:
+							cout << "\e[0;42m";
+							break;
+						case 3:
+							cout << "\e[0;41m";
+							break;
+						case 4:
+							cout << "\e[0;104m";
+							break;
+						case 5:
+							cout << "\e[0;101m";
+							break;
+						case 6:
+							cout << "\e[0;46m";
+							break;
+						case 7:
+							cout << "\e[38;5;240m";
+							break;
+						case 8:
+							cout << "\e[38;5;245m";
+							break;
+					}
+
 				}
 				cout << board[i][j] << " " << "\e[0;0m";
 //				cout << board[i][j] << " ";
@@ -171,6 +203,7 @@ void printBoard(int board[width][height], int lose) {
 int userInput(int* x, int* y, int board[width][height]) {
 	char flag;
 	int temp;
+	int validChord;
 	do {
 		do {
 			cout << "Insert horizontal tile location: ";
@@ -189,16 +222,26 @@ int userInput(int* x, int* y, int board[width][height]) {
 		if (board[*x][*y] != 10 && board[*x][*y] != 9 && board[*x][*y] != 20 && board[*x][*y] != 19) {
 			cout << "Invalid cell selected" << endl;
 		}
-	} while (board[*x][*y] != 10 && board[*x][*y] != 9 && board[*x][*y] != 20 && board[*x][*y] != 19);
+	} while (board[*x][*y] != 10 && board[*x][*y] != 9 && board[*x][*y] != 20 && board[*x][*y] != 19 && !(board[*x][*y] >= 1 && board[*x][*y] <= 8));
 	temp = board[*x][*y];
-	board[*x][*y] = 11;
-	printBoard(board, 0);
-	if (temp >= 19) {
+	if (board[*x][*y] >= 1 && board[*x][*y] <= 8) {
+		board[*x][*y] += 20;
+		printBoard(board, 0);
+	}
+	else {
+		board[*x][*y] = 11;
+		printBoard(board, 0);
+	}
+	if (temp == 19 || temp == 20) {
 		cout << "Are you sure you want to deflag? (y, any other for no) ";
 		cin >> flag;
 		if (flag == 'y' || flag == 'Y') {
 			flag = 'f';
 		}
+	}
+	else if (temp >= 1 && temp <= 8) {
+		cout << "Are you sure you want to chord? (y, any other for no) ";
+		cin >> flag;
 	}
 	else {
 		cout << "Are you sure? (y, f for flag, any other for no) ";
@@ -209,7 +252,7 @@ int userInput(int* x, int* y, int board[width][height]) {
 		flag -= 32;
 	}
 	if (flag == 'F') {
-		if (board[*x][*y] >= 19) {
+		if (board[*x][*y] == 19 || board[*x][*y] == 20) {
 			board[*x][*y] -= 10;
 		}
 		else {
@@ -220,6 +263,28 @@ int userInput(int* x, int* y, int board[width][height]) {
 	else if (flag == 'Y' && board[*x][*y] == 9) {
 		return 1;
 	}
+	else if (flag == 'Y' && temp >= 1 && temp <= 8) {
+		validChord = calcAdjacent(*x, *y, board, 2);
+		if (validChord == board[*x][*y]) {
+			validChord = calcAdjacent(*x, *y, board, 3);
+			if (validChord != 0) {
+				return 1;
+			}
+			for (int i = -1; i < 2; i++) {
+				for (int j = -1; j < 2; j++) {
+					if (*x + i >= 0 && *x + i < width && *y + j >= 0 && *y + j < height) {
+						if (board[*x + i][*y + j] == 10) {
+							board[*x + i][*y + j] = calcAdjacent(*x + i, *y + j, board, 0);
+							expandBoard(*x + i, *y + j, board);
+						}
+					} 
+				}
+			}
+		}
+		else {
+			cout << "Invalid chord! not enough flags" << endl;
+		}
+	}
 	else if (flag != 'Y') {
 		return 2;
 	}
@@ -229,12 +294,18 @@ int calcAdjacent(int x, int y, int board[width][height], int mode) {
 	int count = 0;
 	for (int i = -1; i < 2; i++) {
 		for (int j = -1; j < 2; j++) {
-			if (x + i >= 0 && x + i < width && y + j >= 0 && y + j < height) { // check for out of bounds and skip calculation for the center point
-				if (mode == 0 && board[x + i][y + j] == 9) {
+			if (x + i >= 0 && x + i < width && y + j >= 0 && y + j < height) { // check for out of bounds
+				if (mode == 0 && (board[x + i][y + j] == 9 || board[x + i][y + j] == 19)) {
 					count++;
 				}
 				else if (mode == 1 && board[x + i][y + j] == 0) {
 					return 1; // i know, i know, jacopini... va bene che compiler optimization tolgono le altre condizioni ma questo è più semplice da leggere
+				}
+				else if (mode == 2 && (board[x + i][y + j] == 20 || board[x + i][y + j] == 19)) {
+					count++;
+				}
+				else if (mode == 3 && board[x + i][y + j] == 9) {
+					count++;
 				}
 			}
 		}
@@ -254,7 +325,9 @@ void expandBoard(int x, int y, int board[width][height]) {
 						if (board[x + i][y + j] == 10 || board[x + i][y + j] == 20) {
 							flag = 1;
 						}
-						board[x + i][y + j] = calcAdjacent(x + i, y + j, board, 0);
+						if (board[x + i][y + j] != 19) {
+							board[x + i][y + j] = calcAdjacent(x + i, y + j, board, 0);
+						}
 					}
 				}
 			}

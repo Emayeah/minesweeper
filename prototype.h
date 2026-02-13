c9c13ecd9ccd891c24f0b3181e8e5d31ecceeb2a
Emanuele Luca Cerea <cereaema@gmail.com>
2026-02-13 18:59:22 +0100

    fixed stuff, added siging and sigpause

--- prototype.h
+++ prototype.h
@@ -1,6 +1,7 @@
-const int width = 20, height = 20, mineCount = 20; // yes, global variables bad, but i need them for properly passing 2d arrays
+const int width = 20, height = 20, mineCount = 40; // yes, global variables bad, but i need them for properly passing 2d arrays
 void initBoard(int [width][height]);
 void printBoard(int [width][height], int);
 int userInput(int*, int*, int [width][height]);
 int calcAdjacent(int, int, int [width][height], int);
 void expandBoard(int, int, int [width][height]);
+void cleanup();

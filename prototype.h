9a7fc1518bdf06251e9fac7eae19cc79e9677f65
Emanuele Luca Cerea <cereaema@gmail.com>
2026-02-14 22:39:30 +0100

    added pg-up pg-down home end for 5 steps making movement slightly quicker

--- prototype.h
+++ prototype.h
@@ -1,4 +1,4 @@
-const int width = 160, height = 80, mineCount = 30; // yes, global variables bad, but i need them for properly passing 2d arrays
+const int width = 20, height = 20, mineCount = 20; // yes, global variables bad, but i need them for properly passing 2d arrays
 void initBoard(int [width][height]);
 void printBoard(int [width][height], int);
 int userInput(int*, int*, int [width][height]);
